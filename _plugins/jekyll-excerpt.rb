09d3df1542ca111a7c70721d48c306daefc547a9
Lian, Cheng <rhythm.mail@gmail.com>
2014-01-09 15:22:43 +0800

    Updated Jekyll plugins

--- _plugins/jekyll-excerpt.rb
+++ _plugins/jekyll-excerpt.rb
@@ -1,8 +1,8 @@
 module Jekyll
   class Post
     alias_method :original_to_liquid, :to_liquid
-    def to_liquid
-      original_to_liquid.deep_merge({
+    def to_liquid(attrs = nil)
+      original_to_liquid(attrs).deep_merge({
         'excerpt' => content.match('<hr class="more docutils" />') ? content.split('<hr class="more docutils" />').first : nil
       })
     end
